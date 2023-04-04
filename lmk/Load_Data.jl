dc2f9b11fe6892f1c53456b5ba13aceaee704bee
Pan Wenpu <55241456+phuawenpu@users.noreply.github.com>
2023-04-04 16:58:38 +0800

    adding in agents and households slowly

--- lmk/Load_Data.jl
+++ lmk/Load_Data.jl
@@ -9,23 +9,23 @@ function load_data(filename)
     #extrapolate lower and upper bounds
     df.min_income .= Int32.(df."1st" - round.((df."2nd" - df."1st")/2)) #extrapolate min point of mean income
     df.percentile10 .= Int32.(df."1st") 
-    # df.percentile15 .= Int32.(round.((df."2nd"- df."1st")/2) + df."1st") #extrapolate midpoint between deciles of income
+    df.percentile15 .= Int32.(round.((df."2nd"- df."1st")/2) + df."1st") #extrapolate midpoint between deciles of income
     df.percentile20 .= Int32.(df."2nd") #rename all the deciles to percentiles, to display in increasing order in dataframe
-    # df.percentile25 .= Int32.(round.((df."3rd"- df."2nd")/2) + df."2nd")
+    df.percentile25 .= Int32.(round.((df."3rd"- df."2nd")/2) + df."2nd")
     df.percentile30 .= Int32.(df."3rd")
-    # df.percentile35 .= Int32.(round.((df."4th"- df."3rd")/2) + df."3rd")
+    df.percentile35 .= Int32.(round.((df."4th"- df."3rd")/2) + df."3rd")
     df.percentile40 .= Int32.(df."4th")
-    # df.percentile45 .= Int32.(round.((df."5th"- df."4th")/2) + df."4th") 
+    df.percentile45 .= Int32.(round.((df."5th"- df."4th")/2) + df."4th") 
     df.percentile50 .= Int32.(df."5th")
-    # df.percentile55 .= Int32.(round.((df."6th"- df."5th")/2) + df."5th")
+    df.percentile55 .= Int32.(round.((df."6th"- df."5th")/2) + df."5th")
     df.percentile60 .= Int32.(df."6th")
-    # df.percentile65 .= Int32.(round.((df."7th"- df."6th")/2) + df."6th")
+    df.percentile65 .= Int32.(round.((df."7th"- df."6th")/2) + df."6th")
     df.percentile70 .= Int32.(df."7th")
-    # df.percentile75 .= Int32.(round.((df."8th"- df."7th")/2) + df."7th")
+    df.percentile75 .= Int32.(round.((df."8th"- df."7th")/2) + df."7th")
     df.percentile80 .= Int32.(df."8th")
-    # df.percentile85 .= Int32.(round.((df."9th"- df."8th")/2) + df."8th")
+    df.percentile85 .= Int32.(round.((df."9th"- df."8th")/2) + df."8th")
     df.percentile90 .= Int32.(df."9th")
-    # df.percentile95 .= Int32.(round.((df."10th"- df."9th")/2) + df."9th")
+    df.percentile95 .= Int32.(round.((df."10th"- df."9th")/2) + df."9th")
     df.percentile100 .= Int32.(df."10th")
     df.max_income .= Int32.(round.((df."10th"- df."9th")/2) + df."10th") #extrapolate max point of mean income 
     return select(df, Not([:"1st", :"2nd", :"3rd", :"4th", :"5th", :"6th", :"7th", :"8th", :"9th", :"10th"]))
--- lmk/main.jl
+++ lmk/main.jl
@@ -1,16 +1,52 @@
-# percent of households being simulated
+using DataFrames
+# percent of total number of households being simulated
 percent_agent = 1
 
-include("./Load_Data.jl")
+struct House #each unit of property in the simulation) 
+    available :: Int32 # [1 -> "vacant_no_owner", 2-> "for_sale", 3-> "for_rent", "for_sale_rent", 4->"occupied_unavailable"]
+    owned_by :: Int32 # id of household who owns it  1->state landlord
+    price_previous :: Int32
+    price_current :: Int32
+end
 
-df = Load_Data.load_data("income.csv")
+struct Agent #each household is the agent of the simulation
+    income_other :: Int32
+    income_rental :: Int32
+    primary_residence :: Int32 #index to House[] array, if 1(state landlord) is homeless
+    properties :: Vector{Int32} #which house ids does this agent own 1 is initial state
+    total_debt :: Int32
+end
+
+include("./Load_Data.jl")
+df = Load_Data.load_data("income.csv") #income data is here in deciles
 vscodedisplay(df)
+num_households = df[nrow(df),:"Number_Households"]
+println("Number of Households: ", num_households)
+sim_agent_samplesize = round(Int32, ceil(percent_agent/100 * num_households)/10)
+sim_agent_population = sim_agent_samplesize * 10  #we require the agent population in multiples of ten to fit the income distribution data in deciles
+sim_house_marketsize = round(Int32, sim_agent_population * 1.1) #create the housing market size with some buffer for growth
+
+agent_list = Agent[]
+no_properties = Vector{Int32}()
+for i in 1:sim_agent_samplesize #generate the first decile
+    min_income = df[nrow(df),:"min_income"]
+    max_income = df[nrow(df),:"percentile10"] - 1
+    push!(agent_list, Agent(rand(min_income:max_income), 0, 1, no_properties, 0))
+end
+
 
 #housing price function:
-
 function house_price(income ,income_low, base_price, price_coeff)
     #price_coeff = 0.0001
     price = (income / income_low) * income * base_price * price_coeff
     return Int64(round(price))
 end
+
+#=
+property_market = House[]
+for i in sim_agent_population
+    price = house_price(df[i,:"I"])
+    push!(property_market, House(1, 1, ))
+=#
+
 
