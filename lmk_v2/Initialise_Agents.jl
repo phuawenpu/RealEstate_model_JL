03ab9f0b417a7c24a729efdb30c556f32c481210
PAN WENPU <phuawenpu@gmail.com>
2023-04-10 15:26:39 +0800

    savings function with low/hi percent savings

--- lmk_v2/Initialise_Agents.jl
+++ lmk_v2/Initialise_Agents.jl
@@ -1,4 +1,4 @@
-module Initialise_Agents_v2
+module Initialise_Agents
 
 using DataFrames
 
@@ -17,70 +17,70 @@ function populate_agents(df, row_number, agent_list)
     for i in 1:sim_samplesize #generate the first decile
         min_income = df[row_number,:"min_income"]
         max_income = df[row_number,:"percentile15"] - 1
-        agent_list[index,1] = Int32(round(rand(min_income:max_income)))
+        agent_list[index,1,1]= Int32(round(rand(min_income:max_income)))
         index += 1
     end
 
     for i in 1:sim_samplesize #generate the second decile
         min_income = df[row_number,:"percentile15"]
         max_income = df[row_number,:"percentile25"] - 1
-        agent_list[index,1] = Int32(round(rand(min_income:max_income)))
+        agent_list[index,1,1]= Int32(round(rand(min_income:max_income)))
         index += 1
     end
 
     for i in 1:sim_samplesize #generate the third decile
         min_income = df[row_number,:"percentile25"]
         max_income = df[row_number,:"percentile35"] - 1
-        agent_list[index,1] = Int32(round(rand(min_income:max_income)))
+        agent_list[index,1,1]= Int32(round(rand(min_income:max_income)))
         index += 1
     end
 
     for i in 1:sim_samplesize #generate the fourth decile
         min_income = df[row_number,:"percentile35"]
         max_income = df[row_number,:"percentile45"] - 1
-        agent_list[index,1] = Int32(round(rand(min_income:max_income)))
+        agent_list[index,1,1]= Int32(round(rand(min_income:max_income)))
         index += 1
     end
 
     for i in 1:sim_samplesize #generate the fifth decile
         min_income = df[row_number,:"percentile55"]
         max_income = df[row_number,:"percentile65"] - 1
-        agent_list[index,1] = Int32(round(rand(min_income:max_income)))
+        agent_list[index,1,1]= Int32(round(rand(min_income:max_income)))
         index += 1
     end
 
     for i in 1:sim_samplesize #generate the sixth decile
         min_income = df[row_number,:"percentile65"]
         max_income = df[row_number,:"percentile75"] - 1
-        agent_list[index,1] = Int32(round(rand(min_income:max_income)))
+        agent_list[index,1,1]= Int32(round(rand(min_income:max_income)))
         index += 1
     end
 
     for i in 1:sim_samplesize #generate the seventh decile
         min_income = df[row_number,:"percentile75"]
         max_income = df[row_number,:"percentile85"] - 1
-        agent_list[index,1] = Int32(round(rand(min_income:max_income)))
+        agent_list[index,1,1]= Int32(round(rand(min_income:max_income)))
         index += 1
     end
 
     for i in 1:sim_samplesize #generate the eigth decile
         min_income = df[row_number,:"percentile85"]
         max_income = df[row_number,:"percentile95"] - 1
-        agent_list[index,1] = Int32(round(rand(min_income:max_income)))
+        agent_list[index,1,1]= Int32(round(rand(min_income:max_income)))
         index += 1
     end
 
     for i in 1:sim_samplesize #generate the ninth decile
         min_income = df[row_number,:"percentile95"]
         max_income = df[row_number,:"percentile100"] - 1
-        agent_list[index,1] = Int32(round(rand(min_income:max_income)))
+        agent_list[index,1,1]= Int32(round(rand(min_income:max_income)))
         index += 1
     end
 
     for i in 1:sim_samplesize #generate the tenth decile
         min_income = df[row_number,:"percentile100"]
         max_income = df[row_number,:"max_income"] 
-        agent_list[index,1] = Int32(round(rand(min_income:max_income)))
+        agent_list[index,1,1]= Int32(round(rand(min_income:max_income)))
         index += 1
     end
 
@@ -89,6 +89,15 @@ function populate_agents(df, row_number, agent_list)
     return nothing
 end
 
-export populate_agents
+
+function savings_agents(agent_list; savings_lo, savings_hi)
+    a_size = size(agent_list)[1]
+    for i in 1:a_size
+        agent_list[a_size + i]= Int32(round(rand(savings_lo:savings_hi)/100 * agent_list[a_size]))
+        
+    end
+end
+
+export populate_agents, savings_agents
 
 end #end module
--- lmk_v2/main.jl
+++ lmk_v2/main.jl
@@ -3,20 +3,22 @@ using DataFrames
 row_number = 23
 
 include("./Load_Income.jl")
-include("./Initialise_Agents_v2.jl")
+include("./Initialise_Agents.jl")
 
 income_df = Load_Income.load_income("./income.csv")
 num_households = income_df[row_number,:"Number_Households"]
 println("Total Number of Households: ", num_households)
 num_households = Int32(round((0.05*num_households)/10)*10)
 println("Total Number of Households SIMULATED: ", num_households)
-agent_list = zeros(Int32,num_households,2)
-agent_list[num_households,1] = 99
+agent_list = zeros(Int32,num_households,3,1)
+Initialise_Agents.populate_agents(income_df, row_number, agent_list)
+#sort the agent_list from lowest to highest income
+sort!(agent_list, dims = 1)
 display(agent_list)
-Int32(round(size(agent_list)[1]/10))
-
-
-Initialise_Agents_v2.populate_agents(income_df, row_number, agent_list)
-
+Initialise_Agents.savings_agents(agent_list; savings_lo = 10, savings_hi = 10)
+Initialise_Agents.expenditure_agents(agent_list)
 display(agent_list)
+
+
+
 
