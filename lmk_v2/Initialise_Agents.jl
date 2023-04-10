7196e8f98fcb1676a111249cfb73c29097d24aeb
PAN WENPU <phuawenpu@gmail.com>
2023-04-10 17:14:06 +0800

    initialising house prices midway

--- lmk_v2/Initialise_Agents.jl
+++ lmk_v2/Initialise_Agents.jl
@@ -1,86 +1,86 @@
 module Initialise_Agents
 
 using DataFrames
+include("./Model_Functions.jl")
+
 
 
 function populate_agents(df, row_number, agent_list)
-    # percent of total number of households being simulated
-    #load income deciles into a dataframe
+    # step through the decile of income in the population, 
+    # generate each individual agent's household income based
+    # on the range within the decile
     # Main.VSCodeServer.vscodedisplay(df)
-    
     sim_samplesize = Int32(round(size(agent_list)[1]/10))
-   
-    #and the list of properties they own/rent
-    
+       
     index = 1
 
     for i in 1:sim_samplesize #generate the first decile
         min_income = df[row_number,:"min_income"]
         max_income = df[row_number,:"percentile15"] - 1
-        agent_list[index,1,1]= Int32(round(rand(min_income:max_income)))
+        agent_list[index]= Int32(round(rand(min_income:max_income)))
         index += 1
     end
 
     for i in 1:sim_samplesize #generate the second decile
         min_income = df[row_number,:"percentile15"]
         max_income = df[row_number,:"percentile25"] - 1
-        agent_list[index,1,1]= Int32(round(rand(min_income:max_income)))
+        agent_list[index]= Int32(round(rand(min_income:max_income)))
         index += 1
     end
 
     for i in 1:sim_samplesize #generate the third decile
         min_income = df[row_number,:"percentile25"]
         max_income = df[row_number,:"percentile35"] - 1
-        agent_list[index,1,1]= Int32(round(rand(min_income:max_income)))
+        agent_list[index]= Int32(round(rand(min_income:max_income)))
         index += 1
     end
 
     for i in 1:sim_samplesize #generate the fourth decile
         min_income = df[row_number,:"percentile35"]
         max_income = df[row_number,:"percentile45"] - 1
-        agent_list[index,1,1]= Int32(round(rand(min_income:max_income)))
+        agent_list[index]= Int32(round(rand(min_income:max_income)))
         index += 1
     end
 
     for i in 1:sim_samplesize #generate the fifth decile
         min_income = df[row_number,:"percentile55"]
         max_income = df[row_number,:"percentile65"] - 1
-        agent_list[index,1,1]= Int32(round(rand(min_income:max_income)))
+        agent_list[index]= Int32(round(rand(min_income:max_income)))
         index += 1
     end
 
     for i in 1:sim_samplesize #generate the sixth decile
         min_income = df[row_number,:"percentile65"]
         max_income = df[row_number,:"percentile75"] - 1
-        agent_list[index,1,1]= Int32(round(rand(min_income:max_income)))
+        agent_list[index]= Int32(round(rand(min_income:max_income)))
         index += 1
     end
 
     for i in 1:sim_samplesize #generate the seventh decile
         min_income = df[row_number,:"percentile75"]
         max_income = df[row_number,:"percentile85"] - 1
-        agent_list[index,1,1]= Int32(round(rand(min_income:max_income)))
+        agent_list[index]= Int32(round(rand(min_income:max_income)))
         index += 1
     end
 
     for i in 1:sim_samplesize #generate the eigth decile
         min_income = df[row_number,:"percentile85"]
         max_income = df[row_number,:"percentile95"] - 1
-        agent_list[index,1,1]= Int32(round(rand(min_income:max_income)))
+        agent_list[index]= Int32(round(rand(min_income:max_income)))
         index += 1
     end
 
     for i in 1:sim_samplesize #generate the ninth decile
         min_income = df[row_number,:"percentile95"]
         max_income = df[row_number,:"percentile100"] - 1
-        agent_list[index,1,1]= Int32(round(rand(min_income:max_income)))
+        agent_list[index]= Int32(round(rand(min_income:max_income)))
         index += 1
     end
 
     for i in 1:sim_samplesize #generate the tenth decile
         min_income = df[row_number,:"percentile100"]
         max_income = df[row_number,:"max_income"] 
-        agent_list[index,1,1]= Int32(round(rand(min_income:max_income)))
+        agent_list[index]= Int32(round(rand(min_income:max_income)))
         index += 1
     end
 
@@ -91,6 +91,8 @@ end
 
 
 function savings_agents(agent_list; savings_lo, savings_hi)
+    # given the household income, randomly choose a rate of savings within a range 
+    # to give a corresponding vector of savings
     a_size = size(agent_list)[1]
     for i in 1:a_size
         agent_list[a_size + i]= Int32(round(rand(savings_lo:savings_hi)/100 * agent_list[a_size]))
@@ -99,6 +101,8 @@ function savings_agents(agent_list; savings_lo, savings_hi)
 end
 
 function expenditure_agents(agent_list)
+    # given the household income and savings amount
+    # work out the household expenditure
     a_size = size(agent_list)[1]
     for i in 1:a_size
         saving = agent_list[i] - agent_list[a_size + i]
@@ -111,6 +115,12 @@ function expenditure_agents(agent_list)
     end
 end
 
-export populate_agents, savings_agents, expenditure_agents
+function price_house_list(house_list)
+    for i in 1:size(house_list)[1]
+        house_list[i] = Model_Functions.house_price(agent_list[i],income_df[row_number,:"min_income"],60000, 0.0005)
+    end
+end
+
+export populate_agents, savings_agents, expenditure_agents, price_house_list
 
 end #end module
--- lmk_v2/main.jl
+++ lmk_v2/main.jl
@@ -1,36 +1,38 @@
 using DataFrames
 
-row_number = 23
-
 include("./Load_Income.jl")
 include("./Initialise_Agents.jl")
+
+
+# row number of the income dataframe to load
+row_number = 23
 
 income_df = Load_Income.load_income("./income.csv")
 num_households = income_df[row_number,:"Number_Households"]
 println("Total Number of Households: ", num_households)
-num_households = Int32(round((num_households)/10)*10)
+num_households = Int32(round((0.05num_households)/10)*10)
 println("Total Number of Households SIMULATED: ", num_households)
-agent_list = zeros(Int32,num_households,3,1)
+agent_list = zeros(Int32,num_households,3)
 Initialise_Agents.populate_agents(income_df, row_number, agent_list)
 #sort the agent_list from lowest to highest income
 sort!(agent_list, dims = 1)
-display(agent_list)
+# display(agent_list)
 Initialise_Agents.savings_agents(agent_list; savings_lo = 25, savings_hi = 42)
 Initialise_Agents.expenditure_agents(agent_list)
 display(agent_list)
 
-no_save = 0
-a_size = size(agent_list)[1]
-    for i in 1:a_size
-        saving = agent_list[i] - agent_list[a_size + i]
-        if saving < 0 
-            agent_list[2* a_size + i] = 0
-            no_save += 1
-        else
-            agent_list[2* a_size + i] = saving
-        end
-        
-    end
+house_list = zeros(Int32,num_households)
 
-    no_save / num_households
+for i in 1:size(agent_list)[1]
+    house_list[i] = Model_Functions.house_price(agent_list[i],income_df[row_number,:"min_income"],60000, 0.0005)
+end
+
+
+
+#sort the houses_list from lowest to highest price
+sort!(house_list) #, dims = 1)
+
+display(house_list)
+
+
 
