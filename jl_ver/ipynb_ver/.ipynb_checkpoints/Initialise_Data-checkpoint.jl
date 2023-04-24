18af00ebaea8acb7cfc1a5228d6e17c1ffeb421d
Phua Wenpu <phuawenpu@gmail.com>
2023-04-24 16:56:06 +0800

    an hour before submission

---
+++ jl_ver/ipynb_ver/.ipynb_checkpoints/Initialise_Data-checkpoint.jl
@@ -0,0 +1,137 @@
+module Initialise_Data
+
+using DataFrames
+include("./Model_Functions.jl")
+
+
+
+function populate_agents(df, row_number, agent_list)
+    # step through the decile of income in the population, 
+    # generate each individual agent's household income based
+    # on the range within the decile
+    # Main.VSCodeServer.vscodedisplay(df)
+    sim_samplesize = Int32(round(size(agent_list)[1]/8))
+    println("The year is ",df[row_number, :"Year"], " in Singapore")   
+    index = 1
+
+    for i in 1:sim_samplesize #generate the first decile
+        min_income = df[row_number,:"min_income"]
+        max_income = df[row_number,:"percentile15"] - 1
+        agent_list[index]= Int32(round(rand(min_income:max_income)))
+        index += 1
+    end
+
+    for i in 1:sim_samplesize #generate the second decile
+        min_income = df[row_number,:"percentile15"]
+        max_income = df[row_number,:"percentile25"] - 1
+        agent_list[index]= Int32(round(rand(min_income:max_income)))
+        index += 1
+    end
+
+    for i in 1:sim_samplesize #generate the third decile
+        min_income = df[row_number,:"percentile25"]
+        max_income = df[row_number,:"percentile35"] - 1
+        agent_list[index]= Int32(round(rand(min_income:max_income)))
+        index += 1
+    end
+
+    for i in 1:sim_samplesize #generate the fourth decile
+        min_income = df[row_number,:"percentile35"]
+        max_income = df[row_number,:"percentile45"] - 1
+        agent_list[index]= Int32(round(rand(min_income:max_income)))
+        index += 1
+    end
+
+    for i in 1:sim_samplesize #generate the fifth decile
+        min_income = df[row_number,:"percentile55"]
+        max_income = df[row_number,:"percentile65"] - 1
+        agent_list[index]= Int32(round(rand(min_income:max_income)))
+        index += 1
+    end
+
+    for i in 1:sim_samplesize #generate the sixth decile
+        min_income = df[row_number,:"percentile65"]
+        max_income = df[row_number,:"percentile75"] - 1
+        agent_list[index]= Int32(round(rand(min_income:max_income)))
+        index += 1
+    end
+
+    for i in 1:sim_samplesize #generate the seventh decile
+        min_income = df[row_number,:"percentile75"]
+        max_income = df[row_number,:"percentile85"] - 1
+        agent_list[index]= Int32(round(rand(min_income:max_income)))
+        index += 1
+    end
+
+    for i in 1:sim_samplesize #generate the eigth decile
+        min_income = df[row_number,:"percentile85"]
+        max_income = df[row_number,:"percentile95"] - 1
+        agent_list[index]= Int32(round(rand(min_income:max_income)))
+        index += 1
+    end
+
+    for i in 1:sim_samplesize #generate the ninth decile
+        min_income = df[row_number,:"percentile95"]
+        max_income = df[row_number,:"percentile100"] - 1
+        agent_list[index]= Int32(round(rand(min_income:max_income)))
+        index += 1
+    end
+
+    for i in 1:sim_samplesize #generate the tenth decile
+        min_income = df[row_number,:"percentile100"]
+        max_income = df[row_number,:"max_income"] 
+        agent_list[index]= Int32(round(rand(min_income:max_income)))
+        index += 1
+    end
+
+end #end populate_agent
+
+
+function savings_agents(agent_list; savings_lo, savings_hi)
+    # given the household income, randomly choose a rate of savings within a range 
+    # to give a corresponding vector of savings
+    a_size = size(agent_list)[1]
+    for i in 1:a_size
+        income = agent_list[i]
+        # println("Income in savings_agent: ", income)
+        savings = Int32(round(rand(savings_lo:savings_hi)/100 * income))
+        # println("Savings :", savings)
+        agent_list[a_size + i]= savings
+    end
+end #end savings_agents
+
+function expenditure_agents(agent_list; housing_lo, housing_hi)
+    # given the household income and savings amount
+    # work out the household expenditure
+    a_size = size(agent_list)[1]
+    for i in 1:a_size
+        savings = agent_list[a_size + i]
+        if savings < 0 
+            agent_list[2a_size + i] = 0
+            agent_list[3a_size + i] = 0
+        else
+            expenditure = agent_list[2a_size + i] = agent_list[i] - agent_list[a_size + i]
+            agent_list[3a_size + i] = Int32(round(rand(housing_lo:housing_hi)/100 * expenditure))
+        end
+        
+    end
+end #end expenditure_agents
+
+function house_list_price_from_income(income_df, row_number, agent_list,house_list, base_price, price_coeff)
+    #house price is made a function of income
+    for i in 1:size(house_list)[1]
+        house_list[i] = Model_Functions.house_price(agent_list[i],income_df[row_number,:"min_income"],base_price, price_coeff)
+    end
+end #end house_list_price_from_income
+
+function house_list_rental(house_list, interest_rate, inflation_rate, rent_coeff, max_house_price)
+    h_size = size(house_list)[1]
+    for i in 1: h_size
+        house_list[h_size+i] =  Model_Functions.rental_monthly(house_list[i], interest_rate, inflation_rate, rent_coeff, max_house_price)
+        house_list[2h_size+i] = 0 #initialize renter to 0
+    end
+end #end house_list_rental
+
+export populate_agents, savings_agents, expenditure_agents, house_list_price_from_income
+
+end #end module
---
+++ jl_ver/ipynb_ver/Model_Functions.jl
@@ -0,0 +1,82 @@
+#this is where all the prices, mortgages, rents etc.. are determined
+module Model_Functions
+
+#using CUDA
+using Distributions,Random, DelimitedFiles
+
+Random.seed!(123)
+
+#price houses based on input household monthly income
+function house_price(income ,income_low, base_unitprice, price_coeff)
+    #this pricing formula can be improved, so the prices are an exponential function relative to income
+    price = rand(0.97:1.03) * (income / income_low) * income * base_unitprice * price_coeff
+    return Int64(round(price))
+end
+
+#custom kernel function for price-budget probabilities
+function rent_probability_GPU(result_vector, budget, price_vector, price_spread)
+    index_x = (blockIdx().x - 1) * blockDim().x + threadIdx().x
+    stride_x = gridDim().x * blockDim().x
+
+    for i = index_x:stride_x:length(price_vector)
+        @inbounds if (price_vector[i] > 2*budget)
+            result_vector[i]=0
+            continue
+        else
+            @inbounds spread = price_spread * budget
+            @inbounds L = Normal(budget[i],spread)
+            @inbounds p_fit = Float32(pdf(L, budget))
+            @inbounds result_vector[i] = Float16(pdf(L, price_vector[i]) / p_fit)
+        end
+    end
+    return nothing
+end #end rent_probability_GPU
+
+#cpu version 
+function rent_probability_CPU(budget, price, budget_spread)
+    # all inputs are scalar
+    # using a normal distribution to approximate consumer behaviour
+    # however the hunch is consumer behaviour is more like a skewed distribution
+    # i.e. given the same distance from central price, greater preference for low prices than high prices
+    #=if (price > 2 * budget)
+        rent_probability = 0.0
+        return Float32(rent_probability)
+    else=# ##optimisation for over-budget not used
+        L = Normal(budget,budget_spread*budget)
+        p_fit = Float32(pdf(L, budget))
+        rent_probability = Float32(pdf(L, price) / p_fit)
+        return rent_probability
+    #end ##optimisation for low probability removed
+end #end rent_probability_CPU
+
+#monthly mortgage payment given ANNUAL % interest rate, principal owed, 
+# term i.e. number of repayment months
+function mortgage_monthly(;r, P, N)
+
+    if r ≈ 0.0 #in interest free world, just principle / term
+        return Int32(round(P/N))
+    else
+        r = r / 100 / 12
+        c = r*P / 1-(1+r)^(-N)
+    return Int32(round(c))
+    end
+end
+
+#rent price estimator is based on mortgage
+function rental_monthly(house_price, interest_rate, inflation_rate, rent_coeff, max_house_price)
+    # rental is simply an assumed 30 year term mortgage + inflation
+    house_price_ratio = (max_house_price / house_price) 
+    tenure_corrected = Int32(round((12*30)-house_price_ratio)+1)
+    #println("tenure_corrected: ",tenure_corrected, " for house price: ", house_price)
+    rental = mortgage_monthly(r=interest_rate, P = house_price, N=tenure_corrected) * (1+inflation_rate/100)
+    # println(house_price, " rental is: ", Int32(round(rental)))
+    return Int64(round(rental))
+end #end rental_monthly
+
+function saveCSV(filename, data)
+    writedlm(filename, data, ",")
+end
+
+export house_price, rent_probability_CPU ,rent_probability_GPU, mortgage_monthly, rental_monthly, saveCSV
+
+end #end module
